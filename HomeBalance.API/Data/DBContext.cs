b6ee11d169f285f582fa7d3c77f34cc03ddbb988
vladimir.martell@gmail.com <vladimir.martell@gmail.com>
2020-03-10 22:00:24 -0400

    Initial Commit

---
+++ HomeBalance.API/Data/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using HomeBalance.API.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeBalance.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base (options)
+        {
+
+        }
+
+        public DbSet<Cuenta> Cuentas { get; set; }
+
+
+
+    }
+}
---
+++ HomeBalance.API/Model/Cuenta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeBalance.API.Model
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public int RoutingNumber { get; set; }
+        public String BankName { get; set; }
+        public String AccountType { get; set; }
+        public int Balance { get; set; }
+
+        private double[] historyBalance;
+        
+
+        
+
+
+
+
+    }
+}
